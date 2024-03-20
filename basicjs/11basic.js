78760c6434a76908bc7874e27dedc83fb7e2d213
Vinay Kumar Swami <142449446+Vinaykumarswami@users.noreply.github.com>
2024-03-20 08:20:17 +0000

    container config done

---
+++ basicjs/11basic.js
@@ -0,0 +1,22 @@
+const name="vinay";
+const repoCount=50;
+console.log(name + repoCount +" value");
+
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameNmae="vinay swami";
+console.log(gameNmae[2]);
+console.log(gameNmae.length)
+const newString=gameNmae.substring(0,4);
+console.log(newString);
+
+const anotherString=gameNmae.slice(-8,4);
+console.log(anotherString);
+
+const newString1="    vinay    ";
+console.log(newString1);
+console.log(newString1.trim());
+const url="https://vinay.com/vinayswami2020@gmail.com";
+console.log(url);
+console.log(url.replace('2020@','2021@'));
