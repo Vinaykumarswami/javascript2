44d4b2373baa571f1f1890642f73df91ecf27e58
Vinay Kumar Swami <142449446+Vinaykumarswami@users.noreply.github.com>
2024-03-20 07:46:42 +0000

    container config done

---
+++ basicjs/10basic.js
@@ -0,0 +1,5 @@
+let myRepositoriName="java basic code with chai with java script";
+let newRepositoriesName= myRepositoriName;
+myRepositoriName="java basic with apna college ";
+console.log(newRepositoriesName);
+console.log(myRepositoriName);
