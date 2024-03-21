f2f4ad80cd053e6d08bc12062d501e67564ca0a1
Vinay Kumar Swami <142449446+Vinaykumarswami@users.noreply.github.com>
2024-03-21 07:55:47 +0000

    container config done

---
+++ basic02js/array14basic.js
@@ -0,0 +1,14 @@
+const myArr=[1,2,3,4,5];
+
+console.log(myArr);
+const myArr2= new Array(0,1,2,3,4,5);
+console.log(myArr2);
+
+//array method
+myArr.push(6);
+myArr2.push(6);
+
+
+
+console.log(myArr);
+console.log(myArr2);
