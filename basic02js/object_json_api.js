7009d432d12e69327b06bee6c094ab548f8ba545
Vinay Kumar Swami <142449446+Vinaykumarswami@users.noreply.github.com>
2024-03-21 11:47:47 +0000

    json

---
+++ basic02js/object_json_api.js
@@ -0,0 +1,23 @@
+const course={
+    coursenmae:"js in hindi",
+    price: "999",
+    courseInstructor:"vinay"
+}
+
+//course.courseInstructor
+const{courseInstructor: insturctor}=course;
+// console.log(courseInstructor);
+console.log(insturctor);
+
+//react 
+// const navbar=({company}) => {
+
+// }
+// NavigationPreloadManager(company="vinay");
+
+//json declartion
+// {
+//     name:"vinay",
+//     coursename:"js in hindi",
+//     price:"free"
+// }
